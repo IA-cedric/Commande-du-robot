03344f898fce765745ecc79c1a74e61e1a8cd988
ruyters <55392550+ruyters@users.noreply.github.com>
2019-12-04 10:18:27 +0100

    Add files via upload

--- CC.java
+++ CC.java
@@ -1,4 +1,4 @@
-package robotv2;
+package robotV2;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,6 +12,13 @@ import lejos.robotics.Color;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.MeanFilter;
 
+
+/**
+ * Le but de cette classe est de renvoyer la couleur captee par le capteur couleur afin de pouvoir se situer sur la table de jeu.
+ * Cette classe va etre utilisee par : classe Robot 
+ * @see robotV2.CCC
+ *
+ */
 public class CC extends EV3ColorSensor {
 	private double[] tabDoubleBlue = new double[3];
 	private double[] tabDoubleRed = new double[3];
@@ -25,10 +32,45 @@ public class CC extends EV3ColorSensor {
 
 	private SampleProvider average = new MeanFilter(getRGBMode(), 1);
 
+	/**
+	 * classe des couleurs utiles au robot gerees par un type enumere.
+	 * 
+	 */
 	public enum Couleur {
-		Jaune, Vert, Gris, Noir, Rouge, Blanc, Bleu;
+		/**
+		 * type enumere correspondant à la couleur jaune
+		 */
+		Jaune,
+		/**
+		 * type enumere correspondant à la couleur vert
+		 */
+		Vert, 
+		/**
+		 * type enumere correspondant à la couleur gris
+		 */
+		Gris,
+		/**
+		 * type enumere correspondant à la couleur noir
+		 */
+		Noir,
+		/**
+		 * type enumere correspondant à la couleur rouge
+		 */
+		Rouge,
+		/**
+		 * type enumere correspondant à la couleur blanc
+		 */
+		Blanc, 
+		/**
+		 * type enumere correspondant à la couleur bleu
+		 */
+		Bleu;
 	}
 	
+	/**
+	 * Ce constructeur permet d initialiser le port.
+	 * @param p correspond au port du capteur couleur. 
+	 */
 	public CC(Port p) {
 		super(p);
 		try{
@@ -39,14 +81,23 @@ public class CC extends EV3ColorSensor {
 		setFloodlight(Color.WHITE);
 	}
 	
+	/**
+	 * constructeur sans parametre initialisant le port du capteur couleur a S2 par défaut. 
+	 */
 	public CC() {
 		this(LocalEV3.get().getPort("S2"));
 	}
 
-	//initialise les vecteurs
+	
+	/**
+	 * Cette methode initialise les vecteurs necessaires a la détection des couleurs : dans un premier temps elle
+	 * ouvre un flux en lecture du fichier de CCC(calibration capteur couleur) ou sont presentes les valeurs RGB sous forme 
+	 * de properties pour chaque couleurs, normalise entre 0 et 1.
+	 * @throws IOException si le fichier n existe pas.
+	 */
 	public void init() throws IOException { 
 
-		//ouverture du flux en lecture
+	//ouverture du flux en lecture
 		FileInputStream input = new FileInputStream("colorTESTFINAL.txt"); 
 		Properties prop = new Properties();
 
@@ -65,7 +116,7 @@ public class CC extends EV3ColorSensor {
 		String gray = prop.getProperty("gray");
 		String yellow = prop.getProperty("yellow");
 
-	// renvoie un tableau de string sous forme : "blue" ,"blue[0]", "blue[1]", "blue[2]"
+	// renvoie un tableau de string sous forme : "blue" ="blue[0]", "blue[1]", "blue[2]"
 		String[] blueDetail = blue.split(",");
 		String[] redDetail = red.split(",");
 		String[] greenDetail = green.split(",");
@@ -106,18 +157,21 @@ public class CC extends EV3ColorSensor {
 	}
 
 	
-	//METHODE RENVOIE LE INT CORRESPONDANT A LA COULEUR
 	
-	//public Couleur getColor() {
+
+	/**
+	 * Cette methode recupere les donnees issues du capteur au format RGB. Des valeur est fait un scalaire par rapport 
+	 * a chaque couleur presente dans le fichier genere par CCC(calibration du capteur couleur)
+	 * @return couleur detecte
+	 */
+	
 	public Couleur getColor() {
 		
 	//récupération des données issues du capteur
-		float[] sample = new float[average.sampleSize()]; //création d'un tableau d'échantillons 
-		//System.out.println("\nPress enter to detect a color...");
-		//Button.ENTER.waitForPressAndRelease();
+		float[] sample = new float[average.sampleSize()]; 
 		average.fetchSample(sample, 0);
 		double minscal = Double.MAX_VALUE;
-		Couleur color = null ;
+		
 
 
 		//création d'un tableau contenant les scalaires des couleurs
@@ -139,48 +193,37 @@ public class CC extends EV3ColorSensor {
 		tabScalaires[5]=scalaireGray;
 		tabScalaires[6]=scalaireYellow;
 
-		
-		//faire un type énuméré
+
 		Couleur[] tabcolString={Couleur.Bleu,Couleur.Rouge,Couleur.Vert,Couleur.Noir,Couleur.Blanc,Couleur.Gris,Couleur.Jaune};
 		
-		
+		Couleur color = null;
 		//calcule le scalaire en fonction du fichier de calibration et des données captées
-		
 		for(int i=0; i<tabScalaires.length ;i++) {
 			if (tabScalaires[i]<minscal) {
 				minscal=tabScalaires[i];
 				color = tabcolString[i];
+				System.out.println(color);
 		}  	
 	}
 		
 		
-		//retourne le numéro correspondant à la couleur
+		//retourne le type énuméré correspondant à la couleur détectée.
 		return color;
 
 	}
 
 
-	//METHODE DU CALCUL DES SCALAIRES
+
+	/**
+	 * Cette méthode calcul les scalaires entre les donnees de calibrage et les donnees captees.
+	 * @param v1 donnees recuperees (au format RGB) du capteur couleur
+	 * @param v2 donnees du fichier calibration au format RGB
+	 * @return la valeur du scalaire de v1 et v2
+	 */
 	public static double scalaire(float[] v1, double[] v2) { //
 		return Math.sqrt (Math.pow(v1[0] - v2[0], 2.0) +
 				Math.pow(v1[1] - v2[1], 2.0) +
 				Math.pow(v1[2] - v2[2], 2.0));
 	}
 
-
-
-	/**public static void main(String[]args) throws IOException {
-		//CalibrationCapteurCouleur1 calibration = new CalibrationCapteurCouleur1();
-		
-		Port port = LocalEV3.get().getPort("S2");
-		CapteurCouleur colorSensor = new CapteurCouleur(port);
-
-		
-		
-		System.out.println("la couleur est : "+colorSensor.getColor());
-		Button.waitForAnyPress();
-
-		colorSensor.close();
-		
-	}*/
 }
